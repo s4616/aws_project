45f07816b0c0250318c3e798b2ed03dcb2d3494d
s4616 <s4616@naver.com>
2023-05-23 00:43:10 +0000

    20230523

--- 01-Terraform/one-webserver/main.tf
+++ 01-Terraform/one-webserver/main.tf
@@ -3,35 +3,35 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-0c6e5afdd23291f73"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  ami           = "ami-0c6e5afdd23291f73"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id] 
 
   user_data = <<-EOF
-	            #!/bin/bash
-							echo "Hello World" > index.html
-							nohup busybox httpd -f -p 8080 &
-							 EOF
-
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  # tags 추가
   tags = {
     Name = "aws00-example"
   }
 }
 
 resource "aws_security_group" "instance" {
-  name = "aws00-terraform-example-instance"
-
+  name = "aws00-terrafrom-example-instance"
+  
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port    = 8080
+    to_port      = 8080
+    protocol     = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
   }
 }
 
 output "public_ip" {
   value       = aws_instance.example.public_ip
-  description = "The Public IP of the Example Instance"
+  description = "The public IP of the Instance"
 }
 
 
