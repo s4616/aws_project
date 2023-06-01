9060c0631320614bb6008c65b9bd5d305740375b
s4616 <s4616@naver.com>
2023-06-01 17:08:21 +0900

    20230601

--- 01-Terraform/global/s3/backend.tf
+++ 01-Terraform/global/s3/backend.tf
@@ -7,4 +7,17 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  /*
+  backend "s3" {
+    # 이전에 생성한 버킷 이름으로 변경
+    bucket = "aws00-terraform-state"
+    key = "global/s3/terraform.tfstate"
+    region = "ap-northeast-2"
+
+    # 이전에 생성한 다이나모DB 테이블 이름으로 변경
+    dynamodb_table = "aws00-terraform-locks"
+    encrypt = true
+  }
+  */
 }
