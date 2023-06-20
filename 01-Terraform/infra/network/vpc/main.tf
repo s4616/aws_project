b4197807cd6c24f4d083cd7330a7c3cce9386312
s4616 <s4616@naver.com>
2023-06-20 14:39:10 +0900

    20230620

--- 01-Terraform/infra/network/vpc/main.tf
+++ 01-Terraform/infra/network/vpc/main.tf
@@ -1,29 +1,10 @@
-data "aws_vpc" "default" {
-	filter {
-		name   = "tag:Name"
-		values = ["604-vpc"] 
-	}
-}
+resource "aws_vpc" "aws00-vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  instance_tenancy = "default"
 
-data "aws_subnets" "example" {
-	filter {
-		name   = "vpc-id"
-		values = [data.aws_vpc.default.id]
-	}
-}
-
-data "aws_subnet" "example" {
-	for_each = toset(data.aws_subnets.example.ids)
-	id			 = each.value
-}
-
-output "vpc-id" {
-	value = data.aws_vpc.default.id
-}
-output "subnets" {
-	value = data.aws_subnets.example.ids
-}
-output "subnet_cidr_blocks" {
-#	value = values(data.aws_subnet.example)[*].cidr_block
-	value = [for s in data.aws_subnet.example : s.cidr_block]
+  tags = {
+	Name = "aws00-vpc"
+  }
 }
---
+++ 01-Terraform/infra/network/vpc/variable.tf
@@ -0,0 +1,3 @@
+variable "vpc_cidr" {
+    value = "10.0.0.0/16"
+}
