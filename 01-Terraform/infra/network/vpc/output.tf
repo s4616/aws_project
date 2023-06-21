248e6515b87b7aeceaffb645f067db628c2b044a
s4616 <s4616@naver.com>
2023-06-21 10:40:30 +0900

    20230621

--- 01-Terraform/infra/network/vpc/output.tf
+++ 01-Terraform/infra/network/vpc/output.tf
@@ -1,7 +1,15 @@
 output "vpc_id" {
     value = aws_vpc.aws00_vpc.id
 }
-
-output "azs" {
-    value = aws_vpc.aws00_vpc.azs
+output "public_subnet2a" {
+    value = aws_subnet.aws00_public_subnet2a.id
+}
+output "public_subnet2c" {
+    value = aws_subnet.aws00_public_subnet2c.id
+}
+output "private_subnet2a" {
+    value = aws_subnet.aws00_private_subnet2a.id
+}
+output "private_subnet2c" {
+    value = aws_subnet.aws00_private_subnet2c.id
 }
