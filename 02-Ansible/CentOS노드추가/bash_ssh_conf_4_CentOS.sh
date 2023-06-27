4e6a84c70298e06600a9d8034929da0f63499dc6
s4616 <s4616@naver.com>
2023-06-27 11:31:49 +0900

    20230627

--- 02-Ansible/CentOS노드추가/bash_ssh_conf_4_CentOS.sh
+++ 02-Ansible/CentOS노드추가/bash_ssh_conf_4_CentOS.sh
@@ -4,3 +4,4 @@ now=$(date +"%m_%d_%Y")
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_$now.backup
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
+
